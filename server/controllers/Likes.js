4492afd585ea085a4dcb6a4093d8b2b56722ba96
DylanWebCoder <76613977+DylanWebCoder@users.noreply.github.com>
2021-08-30 16:23:37 +0200

    Correct controller Likes

--- server/controllers/Likes.js
+++ server/controllers/Likes.js
@@ -16,6 +16,6 @@ exports.addLike = async (req, res) => {
     res.json({ liked: true });
   } else {
     await Likes.destroy({ where: { PostId: PostId, UserId: UserId } });
+    res.json({ liked: false });
   }
-  res.json({ liked: false });
 };
